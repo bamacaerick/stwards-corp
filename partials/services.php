52652bdf25c599dddcd0c5083ccbc5795e7c5ff9
Jonatan Reyes <jreyes.reyes17715@gmail.com>
2024-05-17 18:08:06 -0600

    Push First Queries

--- partials/services.php
+++ partials/services.php
@@ -1,1 +1,14 @@
-services query
+services query<?php                    
+    query_posts( array("post_type" => "services") );
+    while ( have_posts() ) : the_post();   
+    $foto_servicio = get_the_post_thumbnail_url(get_the_ID(),'full');  
+?>
+   
+    <h1><?php the_title(); ?></h1>
+    <p><?php the_content(); ?></p>
+    <img src="<?php echo $foto_servicio; ?>" alt="">
+    <span> <?php the_field('correo_electronico') ?></span>
+    <span> <?php the_field('telefono') ?></span>
+<?php 
+    endwhile; wp_reset_query();
+?>  
